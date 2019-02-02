8a897dd8ca565285c37e8a188812bd88fbc0fbef
bk2697 <bk2697@tc.columbia.edu>
2019-02-02 12:44:51 -0500

    Removed var data and inserted console.log of HOLA! (JS)

--- js/mycode.js
+++ js/mycode.js
@@ -1,9 +1,1 @@
-//chart
-var data = [
- { type: "Food", value: 1 },
- { type: "Flight", value: 1 },
- { type: "Lodging", value: 1 },
- { type: "Transport", value: 1 },
- { type: "Activities", value: 1 },
- { type: "Extras", value: 1 }
-];
+console.log('HOLA!');
